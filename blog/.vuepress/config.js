41d147ad91615e63bf959d10b26fc6f6e1369f10
Cleaver Barnes <cbarnes@cleaver.ca>
2020-11-17 15:48:01 -0500

    initial commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  title: 'cleaver.ca',
+  description: 'Personal blog of Cleaver Barnes',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions;
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'About',
+        link: '/about-me/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/cleaver',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/cleaverbarnes',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+    searchPlaceholder: 'Search...',
+    nextLinks: true,
+    prevLinks: true,
+  },
+};
